491eca5bc88ffbaa93d70c06b0938a5abefb5517
Buri Balazs <balazs.buri@euedge.com>
2017-05-25 17:10:39 +0200

    debug field + fps counter

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import * as Phaser from 'phaser';
 class SimpleGame {
     game: Phaser.Game;
     logo: Phaser.Sprite;
+    debug: Phaser.Text;
     cursors: Phaser.CursorKeys;
 
     constructor() {
@@ -24,9 +25,16 @@ class SimpleGame {
         this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
         this.logo.anchor.setTo(0.5, 0.5);
         this.cursors = this.game.input.keyboard.createCursorKeys();
+
+        this.debug = this.game.add.text(10,10,'asd', {
+            font:'monospace',
+            fontSize:12,
+            fill:'#FFFFFF'
+        });
     }
 
     update() {
+        this.debug.text = `${this.game.time.fps} fps`;
         this.game.input.update();
 
         if (this.cursors.down.isDown)
