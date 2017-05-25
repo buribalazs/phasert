2265a5be2149bb689fe4605eb04cada7ad23c403
Buri Balazs <balazs.buri@euedge.com>
2017-05-25 16:37:48 +0200

    refactor

--- src/index.ts
+++ src/index.ts
@@ -5,39 +5,41 @@ import 'p2';
 import * as Phaser from 'phaser';
 
 class SimpleGame {
-  game: Phaser.Game;
-  logo: Phaser.Sprite;
-  cursors: Phaser.CursorKeys;
+    game: Phaser.Game;
+    logo: Phaser.Sprite;
+    cursors: Phaser.CursorKeys;
 
-  constructor() {
-    this.game = new Phaser.Game("100%", "100%", Phaser.AUTO, "content", this);
-  }
+    constructor() {
+        this.game = new Phaser.Game(1920, 1080, Phaser.AUTO, "content", this);
+    }
 
-  preload() {
-    this.game.load.image("logo", "./assets/images/mushroom2.png");
-    this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-  }
+    preload() {
+        this.game.stage.backgroundColor = "#261c2f";
+        this.game.time.advancedTiming = true;
+        this.game.load.image("logo", "./assets/images/mushroom2.png");
+        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    }
 
-  create() {
-    this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
-    this.logo.anchor.setTo(0.5, 0.5);
-    this.cursors = this.game.input.keyboard.createCursorKeys();
-  }
+    create() {
+        this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
+        this.logo.anchor.setTo(0.5, 0.5);
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    }
 
-  update() {
-    this.game.input.update();
+    update() {
+        this.game.input.update();
 
-    if (this.cursors.down.isDown)
-      this.logo.position.y += 10;
-    if (this.cursors.up.isDown)
-      this.logo.position.y -= 10;
-    if (this.cursors.left.isDown)
-      this.logo.position.x -= 10;
-    if (this.cursors.right.isDown)
-      this.logo.position.x += 10;
-  }
+        if (this.cursors.down.isDown)
+            this.logo.position.y += 10;
+        if (this.cursors.up.isDown)
+            this.logo.position.y -= 10;
+        if (this.cursors.left.isDown)
+            this.logo.position.x -= 10;
+        if (this.cursors.right.isDown)
+            this.logo.position.x += 10;
+    }
 }
 
 window.onload = () => {
-  const game = new SimpleGame();
+    const game = new SimpleGame();
 };
